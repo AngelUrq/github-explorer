d9702c579298ef8df35aa60e876b4ea94458db7d
AngelUrq <angel4441916@gmail.com>
2020-07-09 00:45:22 -0400

    user-list: user card added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { UserListComponent } from './components/users/user-list/user-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: UserListComponent },
+  { path: 'repos', component: UserListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
+import { UserCardComponent } from './components/users/user-card/user-card.component';
+import { UserListComponent } from './components/users/user-list/user-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserCardComponent,
+    UserListComponent
   ],
   imports: [
     BrowserModule,
